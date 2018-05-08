ae8e52a765cbfb80ad0588d35845f9416a112d22
aliaksandra.lianko <Aliaksandra_Lianko@epam.com>
2018-05-08 16:42:15 +0300

    Fixed waits

--- src/test/java/scenarios/MyNominationsTest.java
+++ src/test/java/scenarios/MyNominationsTest.java
@@ -1,6 +1,9 @@
 package scenarios;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.Preconditions;
@@ -11,15 +14,15 @@ public class MyNominationsTest extends Preconditions {
     @Test
     public void verifyMyNominationsPage() throws InterruptedException{
         driver.navigate().to("https://test-web1-17.corp.globoforce.com/microsites/t/home?client=recipientbased&setCAG=true");
-        Thread.sleep(5000);
+        waitElementPresent(By.name("username"));
         driver.findElement(By.name("username")).sendKeys("norma_nominator");
         driver.findElement(By.name("password")).sendKeys("pass");
         driver.findElement(By.id("signIn-button")).click();
-        Thread.sleep(3000);
+        waitElementPresent(By.xpath("//*[@id=\"navBar\"]//a[contains(@href,'dashboard')]"));
         driver.findElement(By.xpath("//*[@id=\"navBar\"]//a[contains(@href,'dashboard')]")).click();
-        Thread.sleep(3000);
+        waitElementPresent(By.xpath("//*//a[contains(@href,'MyNominations')]"));
         driver.findElement(By.xpath("//*//a[contains(@href,'MyNominations')]")).click();
-        Thread.sleep(3000);
+        waitElementPresent(By.className("nominations-title"));
         Assert.assertEquals(driver.findElement(By.className("nominations-title")).getText(),"People You Have Recognized");
         Assert.assertEquals(driver.findElement(By.xpath("//*[@class='a-table a-table-nominations']/thead/tr/th[1]")).getText(),"Date");
         Assert.assertEquals(driver.findElement(By.xpath("//*[@class='a-table a-table-nominations']/thead/tr/th[2]")).getText(),"Award Type");
@@ -27,7 +30,7 @@ public class MyNominationsTest extends Preconditions {
         Assert.assertEquals(driver.findElement(By.xpath("//*[@class='a-table a-table-nominations']/thead/tr/th[4]")).getText(),"Status");
         Assert.assertEquals(driver.findElement(By.xpath("//*[@class='a-table a-table-nominations']/thead/tr/th[5]")).getText(),"Award Amount");
         driver.findElement(By.xpath("//*[@class='detailsLink a-tooltip-target']")).click();
-        Thread.sleep(3000);
+        driver.findElement(By.xpath("//*[@id='panel']/div/div/div/h1")).click();
         Assert.assertEquals(driver.findElement(By.xpath("//*[@id='panel']/div/div/div/h1")).getText(),"Nomination Details");
         Assert.assertEquals(driver.findElement(By.xpath("//*[@id='panel']/div/div/div[2]/table[1]/tbody/tr[1]/th[1]")).getText(),"Date Nominated");
         //Assert.assertEquals(driver.findElement(By.xpath("//*[@id='panel']/div/div/div[2]/table[1]/tbody/tr[1]/td[1]")).getText(),"Date");
@@ -69,7 +72,6 @@ public class MyNominationsTest extends Preconditions {
 
 
     }
-
 
 
 }
--- src/test/java/utils/Preconditions.java
+++ src/test/java/utils/Preconditions.java
@@ -1,16 +1,25 @@
 package utils;
 
 import org.omg.Messaging.SYNC_WITH_TRANSPORT;
+import org.openqa.selenium.By;
+import org.openqa.selenium.StaleElementReferenceException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Optional;
 import org.testng.annotations.Parameters;
 
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
 public class Preconditions {
+    private static final int implicitTimeout = 15;
     protected WebDriver driver;
     @Parameters({"browser"})
     @BeforeClass
@@ -18,20 +27,36 @@ public class Preconditions {
         if (browser.equalsIgnoreCase("chrome")) {
             System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\chromedriver.exe");
             driver = new ChromeDriver();
+            driver.manage().timeouts().implicitlyWait(implicitTimeout, TimeUnit.SECONDS);
         }
         else if (browser.equalsIgnoreCase("firefox")) {
             System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\geckodriver.exe");
             driver = new FirefoxDriver();
+            driver.manage().timeouts().implicitlyWait(implicitTimeout, TimeUnit.SECONDS);
         }
         else if (browser.equalsIgnoreCase("ie")) {
             System.setProperty("webdriver.chrome.driver", ".\\src\\test\\resources\\IEDriverServer.exe");
             driver = new InternetExplorerDriver();
+            driver.manage().timeouts().implicitlyWait(implicitTimeout, TimeUnit.SECONDS);
         }
     }
     @AfterClass
     public void closeDriver() {
         if (driver != null)
             driver.quit();
+    }
+    public WebElement waitElementAndClick(WebElement webElement) {
+        return new WebDriverWait(driver, 10)
+                .ignoring(StaleElementReferenceException.class)
+                .pollingEvery(Duration.ofSeconds(1))
+                .until(ExpectedConditions.elementToBeClickable(webElement));
+    }
+
+    public WebElement waitElementPresent(final By by) {
+        return new WebDriverWait(driver, 5)
+                .pollingEvery(Duration.ofSeconds(1))
+                .withMessage("Failed to wait element: " + by)
+                .until(ExpectedConditions.presenceOfElementLocated(by));
     }
 
 }
